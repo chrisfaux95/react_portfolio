9553d0f9f6427a9c8a147c3ad1b6335fdb2a58ad
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-23 14:59:56 -0400

    created header and footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className="footer
+                   mt-1
+                   align-baseline
+                   bg-dark
+                   text-light
+                   text-center">
+            <br />
+            <p>
+                &copy;2020 - Chris Faux.
+            </p>
+        </footer>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <nav className="mb-1
+                navbar
+                navbar-expand
+                navbar-dark
+                bg-primary
+                sticky-top">
+            <a className="navbar-brand mb-1 h1"
+            href="index.html">
+                Chris Faux
+            </a>
+            <ul className="navbar-nav navbar-right ml-md-auto">
+                <li className="nav-item mx-1">
+                    <a className="nav-link btn btn-dark"
+                    href="index.html">
+                        About Me
+                    </a>
+                </li>
+                <li className="nav-item mx-1">
+                    <a className="nav-link btn btn-dark"
+                    href="portfolio.html">
+                        Portfolio
+                    </a>
+                </li>
+                <li className="nav-item mx-1">
+                    <a className="nav-link btn btn-dark"
+                    href="contact.html">
+                        Contact Me
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
