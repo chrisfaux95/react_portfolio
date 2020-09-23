b8b7f81698d3e7c9511974efbc2dda867a890b55
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-23 15:00:26 -0400

    added routes and header/footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Navbar />
+                <Switch>
+                    <Route exact path="/" component={About} />
+                    <Route exact path="/home" component={About} />
+                    <Route exact path="/portfolio" component={Portfolio} />
+                    <Route exact path="/contact" componenet={Contact} />
+                </Switch>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
