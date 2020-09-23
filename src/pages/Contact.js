a7e872c7f26405d006a211d09441822c470e7349
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-23 16:16:05 -0400

    added contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,6 +2,28 @@ import React from 'react';
 
 export default function Contact() {
     return (
-        <div></div>
+        <div className="container mx-auto my-5 rounded">
+            <h2>Contact Me!</h2>
+            <hr />
+            <form className="pb-2">
+                <div className="form-group">
+                    <label for="inputName1">Name</label>
+                    <input type="name" className="form-control" id="inputName1" aria-describedby="nameHelp"
+                        placeholder="Insert Name Here" />
+                </div>
+                <div className="form-group">
+                    <label for="inputEmail1">Email address</label>
+                    <input type="email" className="form-control" id="inputEmail1" aria-describedby="emailHelp"
+                        placeholder="example@website.com" />
+                </div>
+                <div className="form-group">
+                    <label for="inputTextarea1">Message</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+                </div>
+                <div className="">
+                    <div className="btn btn-dark btn-block">SEND</div>
+                </div>
+            </form>
+        </div>
     )
 }
