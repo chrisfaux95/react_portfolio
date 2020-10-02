9c3636021f0074190e3f7bcfaa4aa62f109c9145
chrisfaux95 <chrisfaux95@gmail.com>
2020-10-01 20:58:40 -0400

    forgot file extention

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,7 +11,7 @@ export default function Contact() {
                 </div>
                 <div className="row py-4 mx-1">
                     <div className="col-md-3">
-                        <img src={process.env.PUBLIC_URL + "assets/images/selfie"} alt="Chris Faux" className="img-thumbnail" />
+                        <img src={process.env.PUBLIC_URL + "assets/images/selfie.jpg"} alt="Chris Faux" className="img-thumbnail" />
                     </div>
                     <div className="col-md-8 mr-1">
                         <p>
